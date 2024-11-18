a3f0a6f09a8ddb1b16c9f36d9a7fcda0ef396817
robert.głowacki <r.glowacki@wp.pl>
2024-11-18 19:09:41 +0100

    App test context loads.

---
+++ text-analyzer/src/test/java/com/voundsoftware/analyzer/UserInputTextAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voundsoftware.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInputTextAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
