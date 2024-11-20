dd299ec4a6a5028676b5bb8523c3aa60acac9cc6
glowackirobert <glowackir26@gmail.com>
2024-11-20 20:16:55 +0100

    Integration test added with method to check 2xx and 4xx response.

---
+++ text-analyzer/src/test/java/com/voundsoftware/analyzer/controller/AnalyzerControllerTestIT.java
@@ -0,0 +1,47 @@
+package com.voundsoftware.analyzer.controller;
+
+import static org.hamcrest.Matchers.stringContainsInOrder;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AnalyzerControllerTestIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void givenUserInput_whenTriggerController_thenReturnOccurrencesAndStatus200() throws Exception {
+        String input = "The quick brown fox jumps over the lazy dog";
+        String query = "quick, fox, dog";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/search")
+                .accept(MediaType.APPLICATION_JSON)
+                .param("input", input)
+                .param("query", query))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("input").value(input))
+            .andExpect(jsonPath("values", hasSize(3)))
+            .andExpect(jsonPath("values", stringContainsInOrder("quick", "fox", "dog")));
+    }
+
+    @Test
+    public void givenNoUserInput_whenTriggerController_thenReturn4xxResponse() throws Exception {
+        String query = "user, word";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/search")
+                .accept(MediaType.APPLICATION_JSON)
+                .param("query", query))
+            .andExpect(status().isBadRequest());
+    }
+}
